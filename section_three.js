939c28716be24312ab54b58a200d005a25596f86
Laura <laura.luisa.schmid@gmail.com>
2020-07-19 19:47:54 +0200

    korrigierte Rechtschreibfehler

--- section_three.js
+++ section_three.js
@@ -1,27 +1,27 @@
 var worldmap = document.getElementById("map");
 var tablecells = document.getElementsByClassName("tab");
-var btnextreme = document.getElementById("extreme");
-var btnhigh = document.getElementById("high");
+var btnExtreme = document.getElementById("extreme");
+var btnHigh = document.getElementById("high");
 var table = document.getElementById("table");
 var ext = document.getElementsByClassName("ext");
-var hig = document.getElementsByClassName("hig");
+var high = document.getElementsByClassName("hig");
 
-var countryarray = ['1.Katar', '7.kuwait', '13.indien', '2.israel', '8.saudi arabien', '14.pakistan', '3.libanon', 
+var countryArray = ['1.Katar', '7.kuwait', '13.indien', '2.israel', '8.saudi arabien', '14.pakistan', '3.libanon', 
 '9.eritrea', '15.turkmenistan', '4.iran', '10.ver. arab. emirate', 
 '16.oman', '5.jordanien', '11.san marino', '17.botswana', '6.libyen', '12.bahrain', '', '', '', '', '', '', '', '', '', ''];
-var countryarray2 = ['18.Chile', '27.Afghanistan', '36.Djibouti', '19.Zypern', '28.Spanien', '37.Namibia', '20.Jemen', '29.Algerien',
+var countryArray2 = ['18.Chile', '27.Afghanistan', '36.Djibouti', '19.Zypern', '28.Spanien', '37.Namibia', '20.Jemen', '29.Algerien',
  '38.Kirgistan', '21.Andorra', '30.Tunesien', '39.Niger', '22.Marokko', '31.Syrien', '40.Nepal', '23.Belgien', '32.Türkei', '41.Portugal',
  '24.Mexiko', '33.Albanien', '42.Irak','25.Usbekistan', '34.Armenien', '43.Ägypten', '26.Griechenland', '35.Burkina Faso', '44.Italien'];
 
-modifyMap(countryarray,hig,ext,btnextreme,btnhigh);
+modifyMap(countryArray,high,ext,btnExtreme,btnHigh);
 
-btnextreme.addEventListener("click", function () {
-  modifyMap(countryarray,hig,ext,btnextreme,btnhigh);
+btnExtreme.addEventListener("click", function () {
+  modifyMap(countryArray,high,ext,btnExtreme,btnHigh);
   table.style.animation = "pop 0.8s forwards";
 });
 
-btnhigh.addEventListener("click", function () {
-  modifyMap(countryarray2,ext,hig,btnhigh,btnextreme);
+btnHigh.addEventListener("click", function () {
+  modifyMap(countryArray2,ext,high,btnHigh,btnExtreme);
   table.style.animation = "pop2 0.8s forwards";
 });
 
